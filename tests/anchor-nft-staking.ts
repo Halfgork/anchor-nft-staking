23f6e07e49438b1f57647ea62846b7a159169928
tahir <gorkemsakalak@gmail.com>
2022-12-21 20:13:13 +0300

    test: including new tests

--- tests/anchor-nft-staking.ts
+++ tests/anchor-nft-staking.ts
@@ -4,10 +4,13 @@ import { AnchorNftStaking } from "../target/types/anchor_nft_staking";
 
 describe("anchor-nft-staking", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
 
   const program = anchor.workspace.AnchorNftStaking as Program<AnchorNftStaking>;
 
+  const wallet = anchor.workspace.AnchorNftStaking.provider.wallet
+  
   it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
